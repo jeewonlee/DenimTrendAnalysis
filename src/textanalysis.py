402e496b8003df726ddd1c8096b600a961070dd8
jeewonlee <jee.won.lee@hotmail.com>
2016-11-17 15:35:51 -0800

    extend stopwords

--- src/textanalysis.py
+++ src/textanalysis.py
@@ -74,7 +74,7 @@ def topic_modeling(df, topics):
     # topics = 2
     #df=pd.read_csv(filename, delimiter=';')
     stop_words = get_stop_words('en')
-    stop_words.extend['nigga','wear', 'denim','today','tomorrow'] 
+    stop_words.extend(['nigga','wear', 'denim','today','tomorrow'])
 
     vectorizer = TfidfVectorizer(stop_words=stop_words)
     X = vectorizer.fit_transform(df['text'].fillna(''))
