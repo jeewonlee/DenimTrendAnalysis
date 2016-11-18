dab679a3facac4f6954f35811de752e813ae1b32
jeewonlee <jee.won.lee@hotmail.com>
2016-11-18 12:36:18 -0800

    check data

--- src/check_data.py
+++ src/check_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    df = pd.read_csv('../../cleandata/2013Q3.csv', sep=';')
+    df = pd.read_csv('../../cleandata/2013Q1.csv', sep=';')
     print df.columns
     print len(df)
     print df.date.unique()
