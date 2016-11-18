2e4a88d9c20a9f4ea099428f51795ea197649989
jeewonlee <jee.won.lee@hotmail.com>
2016-11-17 18:35:30 -0800

    try 100

--- src/topicmodeling.py
+++ src/topicmodeling.py
@@ -21,7 +21,7 @@ def topic_modeling(df, topics):
 
 def vec(df):
     stop_words = get_stop_words('en')
-    stop_words.extend(['saturdayonline','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every'])
+    stop_words.extend(['saturdayonline','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every','pair'])
 
     vectorizer = TfidfVectorizer(stop_words=stop_words, ngram_range=(1,2))
     X = vectorizer.fit_transform(df['text'].fillna(''))
@@ -100,22 +100,22 @@ if __name__ == '__main__':
     #df = pd.read_csv('../../data/cleandata/2013Q1_temp.csv', delimiter=';')
     vectorizer, X, features = vec(df)
     #run_kmean(vectorizer, X, features, 20)
-    nmf_words = run_nmf(vectorizer, X, features, 50)
-    svd, n_topics, svd_words = run_SVD(vectorizer, X, features, 50)
+    nmf_words = run_nmf(vectorizer, X, features, 100)
+    svd, n_topics, svd_words = run_SVD(vectorizer, X, features, 100)
     svd_val(n_topics, svd)
-    #lda_words = run_LDA(vectorizer, X, features, 20)
+    lda_words = run_LDA(vectorizer, X, features, 100)
 
-    all_words = nmf_words+svd_words
-    stop_words = get_stop_words('en')
-    stop_words.extend(['saturdayonlin','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every'])
-
-    vectorizer2 = TfidfVectorizer(stop_words=stop_words)
-    X2 = vectorizer.fit_transform(all_words)
-    # vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,3))
-    # X = vectorizer.fit_transform(df['text'].fillna(''))
-    features2 = vectorizer.get_feature_names()
-    nmf_words2 = run_nmf(vectorizer2, X2, features2, 10)
-    svd2, n_topics2, svd_words2 = run_SVD(vectorizer2, X2, features2, 10)
+    # all_words = nmf_words+svd_words
+    # stop_words = get_stop_words('en')
+    # stop_words.extend(['saturdayonlin','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every','pair'])
+    #
+    # vectorizer2 = TfidfVectorizer(stop_words=stop_words)
+    # X2 = vectorizer.fit_transform(all_words)
+    # # vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,3))
+    # # X = vectorizer.fit_transform(df['text'].fillna(''))
+    # features2 = vectorizer.get_feature_names()
+    # nmf_words2 = run_nmf(vectorizer2, X2, features2, 10)
+    # svd2, n_topics2, svd_words2 = run_SVD(vectorizer2, X2, features2, 10)
 
     #topic_modeling(df, 20)
     # print "\n2014 second quarter"
