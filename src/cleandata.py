1a8e49fc29f5babd6e30c7b3c58a51915e0d0e1a
jeewonlee <jee.won.lee@hotmail.com>
2016-11-18 11:22:15 -0800

    lda fix

--- src/cleandata.py
+++ src/cleandata.py
@@ -8,7 +8,7 @@ from collections import Counter
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram
 #from sklearn.decomposition import NMF, LatentDirichletAllocation
-#import jw_tokenize as tw
+import jw_tokenize as tw
 from langdetect import detect
 #from stop_words import get_stop_words
 
@@ -68,4 +68,4 @@ if __name__ == '__main__':
     ads_df, ads_index = filtering_ads(noise_df)
     nonen_df, nonen_index, lan_lst = filter_nonen(ads_df)
     final, bad_index =remove_badword(nonen_df)
-    final.to_csv('../../cleandata/2013Q1.csv', sep=';')
+    cd final.to_csv('../../cleandata/2013Q1.csv', sep=';')
