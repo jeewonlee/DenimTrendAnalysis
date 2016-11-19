ee3478eec5e166e3dc7d8534b0c0fd86c32ded72
jeewonlee <jee.won.lee@hotmail.com>
2016-11-18 16:55:03 -0800

    2016 q2

--- src/check_data.py
+++ src/check_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    print "2015 q4"
-    df = pd.read_csv('../../cleandata/2015Q4.csv', sep=';')
+    print "2016 q1"
+    df = pd.read_csv('../../cleandata/2016Q1.csv', sep=';')
     print df.columns
     print len(df)
     print df.date.unique()
--- src/cleandata.py
+++ src/cleandata.py
@@ -62,10 +62,10 @@ def remove_badword(df):
     return df.drop(df.index[bad_index]), bad_index
 
 if __name__ == '__main__':
-    df = create_df('../../data/201512.csv','../../data/201601.csv','../../data/201602.csv')
+    df = create_df('../../data/201603.csv','../../data/201604.csv','../../data/201605.csv')
     #df.columns = [u'date', u'text', u'geo', u'mentions', u'hashtags', u'Unnamed: 5']
     noise_df, noise_arr =remove_noise(df)
     ads_df, ads_index = filtering_ads(noise_df)
     nonen_df, nonen_index, lan_lst = filter_nonen(ads_df)
     final, bad_index =remove_badword(nonen_df)
-    final.to_csv('../../cleandata/2016Q1.csv', sep=';')
+    final.to_csv('../../cleandata/2016Q2.csv', sep=';')
