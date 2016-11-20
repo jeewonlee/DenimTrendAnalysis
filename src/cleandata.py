2c5d1adbf0b12e390e1b18baf06c449f09ab1ac8
jeewonlee <jee.won.lee@hotmail.com>
2016-11-19 17:23:20 -0800

    all

--- src/cleandata.py
+++ src/cleandata.py
@@ -39,7 +39,7 @@ def create_yr_df(filename1, filename2, filename3,filename4):
     df=df.append(df1, ignore_index=True)
     df=df.append(df2, ignore_index=True)
     df=df.append(df3, ignore_index=True)
-    df.to_csv('../../cleandata/2016.csv', sep=';')
+    df.to_csv('../../cleandata/all.csv', sep=';')
     return df
 
 #Remove tweets without word jeans
@@ -91,4 +91,4 @@ if __name__ == '__main__':
     # nonen_df, nonen_index, lan_lst = filter_nonen(ads_df)
     # final, bad_index =remove_badword(nonen_df)
     # final.to_csv('../../cleandata/2016Q4.csv', sep=';')
-    create_yr_df('../../cleandata/2016Q1.csv', '../../cleandata/2016Q2.csv', '../../cleandata/2016Q3.csv', '../../cleandata/2016Q4.csv')
+    create_yr_df('../../cleandata/2013.csv', '../../cleandata/2014.csv', '../../cleandata/2016Q3.csv', '../../cleandata/2015.csv', '../../cleandata/2016.csv)
