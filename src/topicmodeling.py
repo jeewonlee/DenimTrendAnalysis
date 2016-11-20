1e6fd57ecd0aff26d3362fb22221c8a5c09d5b6e
jeewonlee <jee.won.lee@hotmail.com>
2016-11-20 13:42:05 -0800

    first stopwords edit

--- src/topicmodeling.py
+++ src/topicmodeling.py
@@ -22,8 +22,105 @@ def topic_modeling(df, topics):
 
 def vec(df):
     stop_words = get_stop_words('en')
-    stop_words.extend(['sexiaws','qigfa2', 'http','saturdayonline','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every','pair'])
-
+    stop_words.extend(['nigga','niggas''sexiaws','qigfa2', 'http','saturdayonline','nigga','wear', 'denim','today','tomorrow','dick','saturdaynightonline', 'p9', 'romeo', 'playlyitjbyp9romeo','romeoplaylyitj','night', 'day', 'yesterday', 'wearing','tonight','every','pair'])
+    #All tweets
+    #Topic0
+    stop_words.extend(['lol','people','never','work','fuck','im','hot','take'])
+    #topic1
+    stop_words.extend(['order','welcome','reseller','promo'])
+    #topic2
+    stop_words.extend(['keep','can','eyes','meet','ur','closer','holdin'])
+    #topic3
+    stop_words.extend(['play','please','hey','thank', 'thanks','playp9romeo','wewantp9onsno'])
+    #topic4 - none
+    #topic5
+    stop_words.extend(['hate','much','really','decide','fucking','shopping','fucking','absolutely'])
+    #topic6
+    stop_words.extend(['don','floor','lying'])
+    #topic7
+    stop_words.extend(['room','anymore','tell','none'])
+    #topic8
+    stop_words.extend(['new','need','got','buy','get','want','buying','finally'])
+    #topic9
+    stop_words.extend(['find'])
+    #topic10
+    stop_words.extned(['pills','hands','everything','now','pulled','apart'])
+    #topic11
+    stop_words.extend(['feel','feels','shit','right','quick','good'])
+    #topic12
+    stop_words.extend(['finding','none','don','perfectly','anymore'])
+    #topic13
+    stop_words.extend(['want','school','dropped','got'])
+    #topci14
+    stop_words.extend(['got','shes','wants','everyone','everybody','saturday'])
+    #topic15
+    stop_words.extend(['need','pair','pairs'])
+    #topic16
+    stop_words.extend(['don','know','even','anymore','people','understand','care','get','want','think'])
+    #topic17
+    stop_words.extend(['can','wait','belive','work','decide'])
+    #topic18
+    stop_words.extend(['come','dat','yo'])
+    #topic19
+    stop_words.extend(['got','know','exactly','just','finally','ve'])
+    #topic20
+    stop_words.extend(['need'])
+    #topic21
+    stop_words.extend(['need','buy','get','tee'])
+    #topic22
+    stop_words.extend(['trying', 'need','tryna','tell','mean','work','get'])
+    #topic23
+    stop_words.extend(['just','bought','found'])
+    #topic24
+    stop_words.extend(['followed','world'])
+    #topic25
+    stop_words.extend(['put','told','step','jump','gotta','struggle','trying'])
+    #topic26
+    stop_words.extend(['way','playp9romeo','queronotvz','hear','hey'])
+    #topic27
+    stop_words.extend(['like','pairs'])
+    #topic28
+    stop_words.extend(['walked','eyes','burn','made','know', 'like'])
+    #topci29
+    stop_words.extend(['need','buy','shopping','asap','really','go'])
+    #topic30
+    stop_words.extend(['first','time','wore','remember','haven','year','month'])
+    #topic31
+    stop_words.extend(['still','squeeze','perfect','much'])
+    #topci32-none
+    #topic33-none
+    #topic34-none
+    #topic35
+    stop_words.extend(['teenage','dream','hands','put','let','racing','heart','imma'])
+    #topic36
+    stop_words.extend(['look','good','make','nice'])
+    #topcic37,38,39-none
+    #topci40
+    stop_words.extend(['hug','see','thru'])
+    #topic41
+    stop_words.extend(['size','now','heart','defined','went','judged'])
+    #topic42
+    stop_words.extend(['really','uncomfortable','made','realize','want','like'])
+    #topic43
+    stop_words.extend(['find','can','hard','never','will'])
+    #topic44
+    stop_words.extend(['hearing','good','little','tea','kisses','hearin','likes'])
+    #topic45
+    stop_words.extned(['bought','just','rock','said'])
+    #topic46
+    stop_words.extend(['friday','cold','beer','radio','fried','cheicken','just'])
+    #topic47
+    stop_words.extend(['store'])
+    #topic48
+    stop_words.extend(['got','necessary','can'])
+    #topic49
+    stop_words.extend(['eyes','meet','holding','closer','won','ever','meet','close','closer','til','eyes'])
+    # stop_words.extend(['im','never','one','keep','meet','eyes', 'play','room','anymore','new','buy','bought','want','got','buying','finally','everything','feel','feels','can','none','anymore','finding'])
+    # stop_words.extend(['school','dropped','lying','floor','band','everybody','saturday','need','pair','don','know','even','understand'])
+    # stop_words.extend(['can','never','decide','believe','get','got','come','look','like','tell','mean','lol','followed','gotta','hear','love','new','looks','pairs','go','made','remember','still'])
+    # stop_words.extend(['much', 'fuck','imma','bought','really','something','follow','see', 'thats','show','thru','defined','judged','uncomfortable','realize','hate','find','can','trying','looks'])
+    # stop_words.extend(['good','hearing','wears','wear','said','told','right','meet','friday','beer','radio','chicken','fried','now','rihgt','now','necessary','wanna','ever','alone','won','til','holdin','keep','holding'])
+    # stop_words.extend(['hands','now','good','hand','hands','see','let','put','get','quick','apart','seams','pulled','none','anymore','care','tryna','just','playp9romeo','queronotvz','thank'])
     vectorizer = TfidfVectorizer(stop_words=stop_words, ngram_range=(1,2))
     X = vectorizer.fit_transform(df['text'].fillna(''))
     # vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,3))
@@ -33,7 +130,7 @@ def vec(df):
 
 def print_top_words(model, feature_names, n_top_words):
     allwords = []
-    with open('../analysis/analysis01.txt','ab') as f:
+    with open('../analysis/analysisv1.txt','ab') as f:
         f.write("\n\n2013Q1\n")
         f.write(model.__class__.__name__)
         f.write("\n")
@@ -83,20 +180,20 @@ def run_kmean(vectorizer, X, features, topics):
 def run_nmf(vectorizer, X, features, topics):
     #4.NMF
     nmf = NMF(n_components=topics, random_state=1,alpha=.1, l1_ratio=.5)
-    with open('nmf.pkl', 'wb') as output:
+    with open('nmf_v1.pkl', 'wb') as output:
         mat = nmf.fit_transform(X)
         pickle.dump(mat, output, pickle.HIGHEST_PROTOCOL)
-    print("\nTopics in NMF model:")
+    print("\nFirst stopwords edit, Topics in NMF model:")
     #tfidf_feature_names = vectorizer.get_feature_names()
     print_top_words(nmf, features, 20)
     return mat
 
 def run_SVD(vectorizer, X, features, topics):
     svd = TruncatedSVD(n_components=topics)
-    with open('svd.pkl', 'wb') as output:
+    with open('svd_v1.pkl', 'wb') as output:
         mat = svd.fit_transform(X)
         pickle.dump(mat, output, pickle.HIGHEST_PROTOCOL)
-    print("\nTopics in truncated SVD model:")
+    print("\nFirst stopwords edit, Topics in truncated SVD model:")
     print_top_words(svd, features, 20)
     return svd, topics, mat
 
