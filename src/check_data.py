e03bb3669ae8bece5207e1e4b21edaf0128fb88e
jeewonlee <jee.won.lee@hotmail.com>
2016-11-19 17:09:03 -0800

    h

--- src/check_data.py
+++ src/check_data.py
@@ -2,50 +2,50 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    print "2013"
-    df = pd.read_csv('../../cleandata/2013.csv', sep=';')
-    print df.columns
-    print len(df)
-    print df.date.unique()
-    print "2014"
-    df = pd.read_csv('../../cleandata/2014.csv', sep=';')
-    print df.columns
-    print len(df)
-    print df.date.unique()
-    print "2015"
-    df = pd.read_csv('../../cleandata/2015.csv', sep=';')
-    print df.columns
-    print len(df)
-    print df.date.unique()
-    print "2016"
-    df = pd.read_csv('../../cleandata/2016.csv', sep=';')
-    print df.columns
-    print len(df)
-    print df.date.unique()
-    #
-    # print "2013 Q1"
-    # df = pd.read_csv('../../cleandata/2013Q1.csv', sep=';')
+    # print "2013"
+    # df = pd.read_csv('../../cleandata/2013.csv', sep=';')
     # print df.columns
     # print len(df)
     # print df.date.unique()
-    #
-    # print "2013 Q2"
-    # df = pd.read_csv('../../cleandata/2013Q2.csv', sep=';')
+    # print "2014"
+    # df = pd.read_csv('../../cleandata/2014.csv', sep=';')
     # print df.columns
     # print len(df)
     # print df.date.unique()
-    #
-    # print "2013 Q3"
-    # df = pd.read_csv('../../cleandata/2013Q3.csv', sep=';')
+    # print "2015"
+    # df = pd.read_csv('../../cleandata/2015.csv', sep=';')
     # print df.columns
     # print len(df)
     # print df.date.unique()
-    #
-    # print "2013 Q4"
-    # df = pd.read_csv('../../cleandata/2013Q4.csv', sep=';')
+    # print "2016"
+    # df = pd.read_csv('../../cleandata/2016.csv', sep=';')
     # print df.columns
     # print len(df)
     # print df.date.unique()
+
+    print "2016 Q1"
+    df = pd.read_csv('../../cleandata/2016Q1.csv', sep=';')
+    print df.columns
+    print len(df)
+    print df.date.unique()
+
+    print "2016 Q2"
+    df = pd.read_csv('../../cleandata/2016Q2.csv', sep=';')
+    print df.columns
+    print len(df)
+    print df.date.unique()
+
+    print "2016 Q3"
+    df = pd.read_csv('../../cleandata/2016Q3.csv', sep=';')
+    print df.columns
+    print len(df)
+    print df.date.unique()
+
+    print "2016 Q4"
+    df = pd.read_csv('../../cleandata/2016Q4.csv', sep=';')
+    print df.columns
+    print len(df)
+    print df.date.unique()
     #
     # print "2014 Q1"
     # df = pd.read_csv('../../cleandata/2014Q1.csv', sep=';')
